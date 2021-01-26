aa72094a036bf73da6ea81e9d425c400d97a35d8
Charles Fries <charliefries@icloud.com>
2021-01-26 09:52:11 -0800

    Add isSupported check to Firebase service worker, bump version to 1.0.3

--- addon/instance-initializers/register-firebase-service-worker.js
+++ addon/instance-initializers/register-firebase-service-worker.js
@@ -1,10 +1,11 @@
-export function initialize(appInstance) {
-	const firebase = appInstance.lookup('service:firebase');
-	const messaging = firebase.messaging();
-
-	navigator.serviceWorker.ready.then(reg => {
-		return messaging.useServiceWorker(reg);
-	});
+export function initialize(/*appInstance*/) {
+	if (firebase.messaging.isSupported()) {
+		// const firebase = appInstance.lookup('service:firebase');
+		const messaging = firebase.messaging();
+		navigator.serviceWorker.ready.then((reg) => {
+			return messaging.useServiceWorker(reg);
+		});
+	}
 }
 
 export default {
